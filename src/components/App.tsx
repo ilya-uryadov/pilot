60a53b282fe056c7c6e7a037bcdffd0146b02cda
carbonnolio <ynblpek83@gmail.com>
2018-01-13 16:54:06 -0500

    ES6 style for app component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,6 @@ import * as React from 'react';
 export interface AppProps {
 }
 
-export default class App extends React.Component<AppProps, undefined> {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
-}
+const App = (props: AppProps) => <h1>Hello World!</h1>;
+
+export default App;
